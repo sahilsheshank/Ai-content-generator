9b9a4f3317608ed70aedf108719e98679bc2eb00
sahilsheshank <sahil.sheshank@gmail.com>
2025-03-04 22:00:20 +0530

    second commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
-const isProtectedRoute = createRouteMatcher([
-    "/dashboard(.*)"
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    const session = await auth();
+    if (!session.userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+  }
+  return NextResponse.next();
+});
 
-  ]);
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
